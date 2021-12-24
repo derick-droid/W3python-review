c78aef5e25ee29d6a77747bdcb3740b27dd24045
derick-droid <derrickokinda9@gmail.com>
2021-12-24 22:17:48 +0300

    python variable naming reviews

---
+++ variable/multiples/multiples.py
@@ -0,0 +1,23 @@
+"""
+multiple naming values
+"""
+
+# many values to multiple variables
+
+x, y, z = "derrick", "buddy", "don"
+print(x)
+print(y)
+print(z)
+
+# one value to multiple variable
+
+x = y = z = "elephant"
+
+print(x)
+
+# unpacking collection
+animals = ["dog", "cheetah", "wolf"]
+w = e = r = animals
+print(w)
+print(e)
+print(r)
---
+++ variable/names/naming.py
@@ -0,0 +1,29 @@
+"""
+reviewing the naming of the variables
+"""
+
+# legal naming
+
+myvar = "john"
+_myvar = "john"
+MyVar = "john"
+my_var = "john"
+myvar2 = "john"
+
+# camel case naming
+
+MyVarNaming = "john"
+
+#pascal naming
+
+MyVarNamings = "john"
+
+#snake case
+
+my_var_naming = "john"
+
+print(myvar)
+print(my_var_naming)
+print(my_var)
+print(myvar2)
+
